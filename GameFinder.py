57a5e1f2145a22937458f33faf54ddffe6bc9384
Connor T Gray <cgray@debian07.CIS.COM>
2020-01-27 08:40:41 -0600

    yeet

--- GameFinder.py
+++ GameFinder.py
@@ -6,7 +6,16 @@
 """this program is used to organize a game library of sorts"""
 import pickle
 
-
+def edit():
+    print("hey there, sorry there nothing here yet, thanks for checking out my program,this one will add stuff")
+def show_all():
+    print("hey there, sorry there nothing here yet, thanks for checking out my program, this one will show all")
+def title_search():
+    print("hey there, sorry there nothing here yet, thanks for checking out my program, this one will search titles")
+def remove():
+    print("hey there, sorry there nothing here yet, thanks for checking out my program, this will remove stuff")
+def save_data():
+    print("hey there, sorry there nothing here yet, thanks for checking out my program, this will save the data")
 
 keep_going = True
 
@@ -28,20 +37,19 @@ while keep_going:
     
     choice = input("What would you like to do? ")
     if choice == "1":
-        print("hey there, sorry there nothing here yet, thanks for checking out my program,this one will add stuff")
-        #edit()
+        edit()
+        
     elif choice == "2":
-        print("hey there, sorry there nothing here yet, thanks for checking out my program, this one will show all")
-        #show_all()
+        show_all()
+        
     elif choice == "3":
-        print("hey there, sorry there nothing here yet, thanks for checking out my program, this one will search titles")
-        #title_search()
+        title_search()
+        
     elif choice == "4":
-        print("hey there, sorry there nothing here yet, thanks for checking out my program, this will remove stuff")
-        #remove()
+        remove()
+        
     elif choice == "5":
-        print("hey there, sorry there nothing here yet, thanks for checking out my program, this will save the data")
-        #save_data
+        save_data()
 
     elif choice == "Q" or choice == "q":
         print("yeah i get it, theres nothin here yet, come back soon.") 
